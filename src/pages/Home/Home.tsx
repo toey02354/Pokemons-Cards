f66c6fb5bd2367298a9a6de88c382d9922ebbb89
natthaphol_lsc <natthaphol@looksocial.asia>
2022-06-25 14:23:06 +0700

    not success: sort by Id or Name

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,7 @@ interface Pokemons {
 
 const Home = () => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [sortById, setSortById] = useState<boolean>(true);
   const [newCardsIndex, setNewCardsindex] = useState(0);
   const [pokemons, setPokemon] = useState<Pokemons[]>([
     { id: 0, name: "", url: "" },
@@ -20,28 +21,11 @@ const Home = () => {
   ) => {
     setLoading(true);
     let newPoke: Pokemons[] = [];
-    // for (let i = 1; i < 13; i++) {
-    //   await axios
-    //     .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    //     .then((res) => {
-    //       console.log(res.data);
-    //       //   setPokemon(res.data.results);
-    //       newPoke[i] = {
-    //         id: i - 1,
-    //         name: res.data.name,
-    //         url: res.data.sprites.front_default,
-    //       };
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    // }
     while (cardsindex + moreCards < 13 + moreCards) {
       await axios
         .get(`https://pokeapi.co/api/v2/pokemon/${cardsindex + moreCards}`)
         .then((res) => {
-          console.log(res.data);
-          //   setPokemon(res.data.results);
+          //   console.log(res.data);
           newPoke[cardsindex] = {
             id: cardsindex + moreCards - 1,
             name: res.data.name,
@@ -61,15 +45,37 @@ const Home = () => {
     getPokemonsById();
   }, [newCardsIndex]);
 
+  useEffect(() => {
+    console.log(sortById);
+  }, [sortById]);
+
   return (
-    <div className="h-screen w-screen flex flex-col justify-between gap-8 p-8 bg-[#F6F6F7]">
+    <div className="h-screen w-screen flex flex-col justify-between gap-8 p-[3rem] bg-[#F6F6F7]">
       <div>
         <div className="w-full flex flex-row justify-between items-start">
           <div className="flex text-[48px]">All Pokemon!</div>
           <div className="flex gap-4">
             <label className="flex gap-4 justify-center items-center">
-              <input type="radio" name="Sort Name" id="sortName" /> Sort Name
-              <input type="radio" name="" id="" /> Sort ID
+              <input
+                type="radio"
+                name="SortName"
+                id="sortName"
+                checked={!sortById}
+                onChange={() => {
+                  setSortById(false);
+                }}
+              />{" "}
+              Sort Name
+              <input
+                type="radio"
+                name="SortId"
+                id="sortId"
+                checked={sortById}
+                onChange={() => {
+                  setSortById(true);
+                }}
+              />{" "}
+              Sort ID
             </label>
           </div>
         </div>
